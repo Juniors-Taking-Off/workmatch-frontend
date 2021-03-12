ac817e57b483241a8df7a5cf0445b2fc892e806d
emily chachian <emilychachian@hotmail.com>
2021-03-12 15:33:29 -0300

    chore: Updating file structure
    Moving "pages" into "src"

--- pages/index.js
+++
@@ -1,5 +0,0 @@
-function HomePage() {
-    return <div>Testing Next.js</div>
-}
-
-export default HomePage; 
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function HomePage() {
+  return <div>Testing Next.js</div>;
+}
+
+export default HomePage;
